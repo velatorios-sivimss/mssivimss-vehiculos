fe6d4b986c56cf66454bce4f4c0c1771f6c789c6
Domingo Fernández <domingo.fernandez@people-media.com.mx>
2023-06-02 17:07:24 -0600

    se actualiza campos

--- src/main/java/com/imss/sivimss/vehiculos/beans/MttoReporte.java
+++ src/main/java/com/imss/sivimss/vehiculos/beans/MttoReporte.java
@@ -30,60 +30,45 @@ public class MttoReporte {
         Integer tamanio = Integer.valueOf(Integer.parseInt(request.getDatos().get("tamanio").toString()));
         reporteRequest.setTamanio(tamanio.toString());
         reporteRequest.setPagina(pagina.toString());
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
-        queryUtil.select("MV.FEC_REGISTRO",
-                        "VH.DES_PLACAS",
-                        "SMN_ACEITE.DES_NIVEL as DES_NIVEL_ACEITE",
-                        "SMN_AGUA.DES_NIVEL as DES_NIVEL_AGUA",
-                        "SMN_NETRA.DES_NIVEL as DES_NIVEL_NEUMATRASE",
-                        "SMN_NEDEL.DES_NIVEL as DES_NIVEL_NEUMADELA",
-                        "SMN_COMB.DES_NIVEL as DES_NIVEL_COMBUSTIBLE",
-                        "SMN_BATERIA.DES_NIVEL as DES_NIVEL_BATERIA",
-                        "SMN_CODIGO.DES_NIVEL as DES_NIVEL_CODIGOFALLO",
-                        "SMN_LIMPINT.DES_NIVEL as DES_NIVEL_LIMPIEZAINTERIOR",
-                        "SMN_LIMPEXT.DES_NIVEL as DES_NIVEL_LIMPIEZAEXTERIOR",
-                        "VI.ID_MTTOVERIFINICIO",
-                        "VH.ID_VEHICULO",
-                        "MV.ID_MTTOVEHICULAR",
-                        "MV.ID_DELEGACION",
-                        "COUNT(MV.ID_VEHICULO) AS TOTAL_VEHICULOS"
-                )
-                .from("SVT_VEHICULOS VH")
-                .join("SVC_USO_VEHICULO UV", "VH.ID_USOVEHICULO = UV.ID_USOVEHICULO")
-                .leftJoin("SVT_MTTO_VEHICULAR MV", "MV.ID_VEHICULO = VH.ID_VEHICULO")
-                .leftJoin("SVT_MTTO_VERIF_INICIO VI", "VI.ID_MTTOVEHICULAR = MV.ID_MTTOVEHICULAR")
-                .leftJoin("SVC_MTTO_NIVEL SMN_ACEITE","SMN_ACEITE.ID_MTTONIVEL=VI.ID_NIVELACEITE")
-                .leftJoin("SVC_MTTO_NIVEL SMN_AGUA","SMN_AGUA.ID_MTTONIVEL=VI.ID_NIVELAGUA")
-                .leftJoin("SVC_MTTO_NIVEL SMN_NETRA","SMN_NETRA.ID_MTTONIVEL=VI.ID_CALNEUTRASEROS")
-                .leftJoin("SVC_MTTO_NIVEL SMN_NEDEL","SMN_NEDEL.ID_MTTONIVEL=VI.ID_CALNEUDELANTEROS")
-                .leftJoin("SVC_MTTO_NIVEL SMN_COMB","SMN_COMB.ID_MTTONIVEL=VI.ID_NIVELCOMBUSTIBLE")
-                .leftJoin("SVC_MTTO_NIVEL SMN_BATERIA","SMN_BATERIA.ID_MTTONIVEL=VI.ID_NIVELBATERIA")
-                .leftJoin("SVC_MTTO_NIVEL SMN_CODIGO","SMN_CODIGO.ID_MTTONIVEL=VI.ID_CODIGOFALLO")
-                .leftJoin("SVC_MTTO_NIVEL SMN_LIMPINT","SMN_LIMPINT.ID_MTTONIVEL=VI.ID_LIMPIEZAINTERIOR")
-                .leftJoin("SVC_MTTO_NIVEL SMN_LIMPEXT","SMN_LIMPEXT.ID_MTTONIVEL=VI.ID_LIMPIEZAEXTERIOR")
-                .where("VH.IND_ACTIVO = :idEstatus")
-                .setParameter("idEstatus", 1);
+        StringBuilder query=new StringBuilder();
+        query.append("SELECT MV.FEC_REGISTRO,VH.DES_PLACAS,SMN_ACEITE.DES_NIVEL as DES_NIVEL_ACEITE,SMN_AGUA.DES_NIVEL as DES_NIVEL_AGUA,\n" +
+                "SMN_NETRA.DES_NIVEL as DES_NIVEL_NEUMATRASE,SMN_NEDEL.DES_NIVEL as DES_NIVEL_NEUMADELA,SMN_COMB.DES_NIVEL as DES_NIVEL_COMBUSTIBLE,\n" +
+                "SMN_BATERIA.DES_NIVEL as DES_NIVEL_BATERIA,SMN_CODIGO.DES_NIVEL as DES_NIVEL_CODIGOFALLO,SMN_LIMPINT.DES_NIVEL as DES_NIVEL_LIMPIEZAINTERIOR,\n" +
+                "SMN_LIMPEXT.DES_NIVEL as DES_NIVEL_LIMPIEZAEXTERIOR,VI.ID_MTTOVERIFINICIO,VH.ID_VEHICULO,MV.ID_MTTOVEHICULAR,MV.ID_DELEGACION,COUNT(MV.ID_VEHICULO) AS TOTAL_VEHICULOS\n" +
+                "FROM SVT_VEHICULOS VH\n" +
+                "INNER JOIN SVC_USO_VEHICULO UV ON VH.ID_USOVEHICULO = UV.ID_USOVEHICULO\n" +
+                "LEFT JOIN SVT_MTTO_VEHICULAR MV ON MV.ID_VEHICULO = VH.ID_VEHICULO\n" +
+                "LEFT JOIN SVT_MTTO_VERIF_INICIO VI ON VI.ID_MTTOVEHICULAR = MV.ID_MTTOVEHICULAR\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_ACEITE ON SMN_ACEITE.ID_MTTONIVEL=VI.ID_NIVELACEITE\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_AGUA ON SMN_AGUA.ID_MTTONIVEL=VI.ID_NIVELAGUA\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_NETRA ON SMN_NETRA.ID_MTTONIVEL=VI.ID_CALNEUTRASEROS\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_NEDEL ON SMN_NEDEL.ID_MTTONIVEL=VI.ID_CALNEUDELANTEROS\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_COMB ON SMN_COMB.ID_MTTONIVEL=VI.ID_NIVELCOMBUSTIBLE\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_BATERIA ON SMN_BATERIA.ID_MTTONIVEL=VI.ID_NIVELBATERIA\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_CODIGO ON SMN_CODIGO.ID_MTTONIVEL=VI.ID_CODIGOFALLO\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_LIMPINT ON SMN_LIMPINT.ID_MTTONIVEL=VI.ID_LIMPIEZAINTERIOR\n" +
+                "LEFT JOIN SVC_MTTO_NIVEL SMN_LIMPEXT  ON SMN_LIMPEXT.ID_MTTONIVEL=VI.ID_LIMPIEZAEXTERIOR\n" +
+                "WHERE VH.IND_ACTIVO = 1");
+
         if (reporteRequest.getPlaca() != null && reporteRequest.getPlaca().trim().length()>0) {
-            queryUtil.where("VH.DES_PLACAS = :placa")
-                    .setParameter("placa", reporteRequest.getPlaca());
+            query.append(" AND VH.DES_PLACAS =").append("'").append(reporteRequest.getPlaca()).append("'");
         }
-        if (reporteRequest.getFechaInicio() != null && reporteRequest.getFechaFinal() != null) {
-            queryUtil.where("MV.FEC_REGISTRO >= CAST(:fechaInicio AS DATE")
-                    .setParameter("fechaInicio", "'" + reporteRequest.getFechaInicio() + "'");
-            queryUtil.where("MV.FEC_REGISTRO <= CAST(:fechaFinal AS DATE")
-                    .setParameter("fechaFinal", "'" + reporteRequest.getFechaFinal() + "'");
+        if (reporteRequest.getFechaInicio() != null) {
+            query.append(" AND MV.FEC_REGISTRO >= CAST('").append(reporteRequest.getFechaInicio()).append("' AS DATE)");
         }
-        //queryUtil.groupBy("MV.ID_VEHICULO");
-        String query =queryUtil.build();
+        if (reporteRequest.getFechaFinal() != null) {
+            query.append(" AND MV.FEC_REGISTRO >= CAST('").append(reporteRequest.getFechaFinal()).append("' AS DATE)");
+        }
+        query. append(" GROUP BY MV.ID_VEHICULO");
         DatosRequest dr = new DatosRequest();
         Map<String, Object> parametro = new HashMap<>();
-        String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
+        String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes());
         parametro.put(AppConstantes.QUERY, encoded);
         parametro.put("pagina",reporteRequest.getPagina());
         parametro.put("tamanio",reporteRequest.getTamanio());
         request.getDatos().remove("datos");
         dr.setDatos(parametro);
-        logger.info(query);
+        logger.info(query.toString());
         return dr;
     }
 
@@ -182,9 +167,9 @@ public class MttoReporte {
 
         }
         if (reporteRequest.getFechaInicio() != null && reporteRequest.getFechaFinal() != null) {
-            queryUtil.where("MV.FEC_REGISTRO >= CAST(:fechaInicio AS DATE")
+            queryUtil.where("MV.FEC_REGISTRO >= CAST(:fechaInicio AS DATE)")
                     .setParameter("fechaInicio", "'" + reporteRequest.getFechaInicio() + "'");
-            queryUtil.where("MV.FEC_REGISTRO <= CAST(:fechaFinal AS DATE")
+            queryUtil.where("MV.FEC_REGISTRO <= CAST(:fechaFinal AS DATE)")
                     .setParameter("fechaFinal", "'" + reporteRequest.getFechaFinal() + "'");
         }
         queryUtil.groupBy("MV.ID_VEHICULO");
