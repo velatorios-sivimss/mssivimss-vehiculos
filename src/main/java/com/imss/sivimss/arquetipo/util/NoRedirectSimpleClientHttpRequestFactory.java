e0ee0a6f8722671efa0667cad3f0d4d6c2860baa
Omar Sanz <108887969+omarberrielpm@users.noreply.github.com>
2023-06-02 11:11:12 -0600

    Revert "Cambio de numero de mensaje"

---
+++ src/main/java/com/imss/sivimss/arquetipo/util/NoRedirectSimpleClientHttpRequestFactory.java
@@ -0,0 +1,13 @@
+package com.imss.sivimss.arquetipo.util;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+public class NoRedirectSimpleClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
+
+    @Override
+    protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+        super.prepareConnection(connection, httpMethod);
+        connection.setInstanceFollowRedirects(false);
+    }
+}
