effed835f80012603e91f3ffd47923f484bc3d91
oliverPimentelL <oliver.pimentel@people-media.com.mx>
2023-06-02 08:21:17 -0600

    Modifican mensajes por los correctos y formato fecha para calendario

--- src/main/java/com/imss/sivimss/vehiculos/service/DisponibilidadVehiculosService.java
+++ src/main/java/com/imss/sivimss/vehiculos/service/DisponibilidadVehiculosService.java
@@ -16,7 +16,6 @@ public interface DisponibilidadVehiculosService {
 	Response<Object> consultaVehiculoDetallexDia(DatosRequest request, Authentication authentication) throws IOException;
 	Response<Object> consultaOperador(DatosRequest request, Authentication authentication) throws IOException;
 	Response<Object> consultaODS(DatosRequest request, Authentication authentication) throws IOException;
-	Response<Object> consultarVelatorio(DatosRequest request, Authentication authentication) throws IOException;
 	Response<Object> registraEntradaVehiculo(DatosRequest request, Authentication authentication) throws IOException;
 	Response<Object> registraSalidaVehiculo(DatosRequest request, Authentication authentication) throws IOException;
 	Response<Object> generarDocumento(DatosRequest request, Authentication authentication) throws IOException;
